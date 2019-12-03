2663a34cc477b9d8f60be64300b408eaefe8a276
Matteo <mgassend@gmail.com>
2019-12-03 16:45:07 +0100

    added service model, referenced it in Config model, added and updated tests accordingly

--- tests/service.test.js
+++ tests/service.test.js
@@ -1,9 +1,9 @@
 const MongoMemoryServer = require("mongodb-memory-server").MongoMemoryServer
 const mongoose = require('mongoose');
-const configModel = require('../models/Config');
 const serviceModel = require("../models/Service")
+
 const mongod = new MongoMemoryServer();
-describe("Config model test", () => {
+describe("Service model test", () => {
 	beforeAll(async () => {
 		const uri = await mongod.getConnectionString();
 		await mongoose.connect(`${uri}`, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
@@ -13,17 +13,19 @@ describe("Config model test", () => {
 			}
 		});
 	})
-	it("create & save a config", async done => {
-		const validConfig = new configModel({
-			name: "test router",
+	it("create & save a service", async done => {
+		const validConfig = new serviceModel({
+			displayName:"test",
+			name: "test.exe",
+			bandwidth: 200.0
 		});
 		const savedConfig = await validConfig.save();
 		expect(savedConfig._id).toBeDefined();
-		expect(savedConfig.name).toBe("test router");
+		expect(savedConfig.name).toBe("test.exe");
 		return done()
 	})
-	it("create invalid config", async done => {
-		const routerWithoutRequiredFields = new configModel({});
+	it("create invalid service", async done => {
+		const routerWithoutRequiredFields = new serviceModel({ name: 'TekLoon' });
 		let err;
         try {
             const savedrouterWithoutRequiredFields = await routerWithoutRequiredFields.save();
@@ -32,19 +34,6 @@ describe("Config model test", () => {
             err = error
 		}
 		expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-		return done()
-	})
-	it("create a config and add a service to it", async done => {
-		const validConfig = new configModel({
-			name: "test router",
-		});
-		let savedConfig = await validConfig.save();
-		expect(savedConfig._id).toBeDefined();
-		let savedService = await serviceModel({displayName:"test", name:"test.exe", bandwidth:200.0}).save()
-		savedConfig.services.push(savedService)
-		await savedConfig.save()
-		savedConfig = await configModel.findById(savedConfig._id).populate("services")
-		expect(savedConfig.services[0].name).toBe("test.exe")
 		return done()
 	})
 	afterAll(async () => {
