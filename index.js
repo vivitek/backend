54688e6d8d867249ab63b8f1e8fe11b33d40ca5c
Théo Marinho <theo.marinho@epitech.eu>
2019-12-04 13:55:28 +0100

    add config routes tests

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const app = require("./server")
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const serviceRouter = require("./routes/service")
+const serviceConfig = require("./routes/config")
+// imports db client
+const db = require("./db")
 
-app.listen(process.env.PORT || 5000, () => {
-	console.log(`[+] Listening on port ${process.env.PORT || 5000}`)
+// if there is a connection error, do this
+db.on("error", () => {
+	console.error.bind(console, '[-] connection error: ')
 })
+
+// once a connection is established, do this
+db.once("open", () => {
+	console.log("[+] Connection to database established")
+})
+
+app.use(morgan("dev"))
+app.use(cors())
+app.use(bodyParser.json());
+app.use("/service", serviceRouter)
+app.use("/config", serviceConfig)
+
+module.exports = app
