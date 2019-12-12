945697e35bea95cc64675008a81e1360fa98dbc1
Matteo Gassend <mgassend@gmail.com>
2019-12-12 16:01:19 +0100

    updating stuff

--- models/Router.js
+++ models/Router.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose")
 const routerSchema = mongoose.Schema({
 	name: {type: String, required:true},
 	url: {type: String, required:true},
-	config: {type: mongoose.Schema.Types.ObjectId, ref:"config"}
 })
 
 const routerModel = mongoose.model("router", routerSchema)
