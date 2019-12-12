c3176038dcdce2ae30052d6a3e134cf9a15eaa01
Matteo Gassend <mgassend@gmail.com>
2019-12-12 15:53:43 +0100

    fixing stuff

--- models/Router.js
+++ models/Router.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const routerSchema = mongoose.Schema({
 	name: {type: String, required:true},
 	url: {type: String, required:true},
-	config: {type: String}
+	config: {type: mongoose.Schema.Types.ObjectId, ref:"config"}
 })
 
 const routerModel = mongoose.model("router", routerSchema)
