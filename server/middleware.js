561b1bee5454524cdb662781c87540420ede5ba0
Théo MARINHO <theo.marinho@epitech.eu>
2020-04-26 18:13:51 +0200

    add middleware tests

--- server/middleware.js
+++ server/middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 const userModel = require("./models/User")
 
 async function checkAuthentication(req, res, next) {
-	if (process.env.DEBUG) {
+	if (process.env.DEBUG && !req.body.isTestingAuth) {
 		let users = await userModel.find()
 		if (users.length > 0) {
 			req.user = users[0]
