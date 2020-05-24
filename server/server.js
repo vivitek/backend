9ef3e6739b3219b315dceea921f9f7f4e934b926
Matteo <matteo.gassend@epitech.eu>
2020-05-24 13:21:23 +0200

    [feat] added rotating-file-stream package to handle daily log stream changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const rfs = require("rotating-file-stream")
 // routers for services
 const serviceRouter = require("./routes/service");
 const authRouter = require("./routes/auth");
@@ -16,7 +17,18 @@ const eventsRouter = require("./sse/index");
 // configure initial app
 if (!process.env.DEBUG) {
 	const db = require("./db");
+	const pad = num => (num > 9 ? "" : "0") + num;
+	const generator = (time, index) => {
+		if (!time) return "file.log";
 
+		var month = time.getFullYear() + "" + pad(time.getMonth() + 1);
+		var day = pad(time.getDate());
+		var hour = pad(time.getHours());
+		var minute = pad(time.getMinutes());
+
+		return `${month}/${month}${day}-${hour}${minute}-${index}-file.log`;
+	};
+	const logStream = rfs.createStream(generator, { interval: "1d", path: "/logs" });
 	// if there is a connection error, do this
 	db.on("error", () => {
 		console.error.bind(console, "[-] connection error: ");
@@ -26,8 +38,8 @@ if (!process.env.DEBUG) {
 	db.once("open", () => {
 		console.log("[+] Connection to database established");
 	});
+	app.use(morgan("combined", { stream: logStream }));
 	app.use(morgan("dev"));
-
 	// handle ctrl+C and `docker-compose down`
 	process.on("SIGINT", async () => {
 		console.log("[-] Stopping server");
