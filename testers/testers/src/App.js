2fae758aa66291ea31871029243800375e922d45
Matteo <matteo.gassend@epitech.eu>
2020-06-03 08:01:41 +0200

    [fix] added tester for sse events

---
+++ testers/testers/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {MDBContainer} from 'mdbreact'
+import {BrowserRouter} from 'react-router-dom'
+import Nav from './components/Nav';
+import Routes from './Routes';
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <MDBContainer fluid>
+        <Routes />
+      </MDBContainer>
+    </BrowserRouter>
+  );
+}
+
+export default App;
