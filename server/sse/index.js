3ce541eabfe50e8227eeef50802d3413d3791bed
Matteo <matteo.gassend@epitech.eu>
2020-06-10 17:54:23 +0200

    [add] replaced rabbitmq with rethink in sse logic

--- server/sse/index.js
+++ server/sse/index.js
@@ -2,12 +2,10 @@ const router = require("express").Router();
 const broker = require("../messages/index");
 const banModel = require("../models/Ban");
 const {checkAuthentication} = require("../middleware");
+const r = require("rethinkdb");
 router.get("/:id", checkAuthentication, async(req, res) => {
-	const {datetime} = req.query;
-	const sentValues = {};
-	console.log(datetime);
-	sentValues[datetime] = [];
 	let {id} = req.params;
+	const connection = await broker.createConnection();
 	res.writeHead(200, {
 		"Content-Type":"text/event-stream",
 		"Cache-Control": "no-cache",
@@ -22,16 +20,15 @@ router.get("/:id", checkAuthentication, async(req, res) => {
 			});
 		}
 	});
-	broker.readConnections(`${id}`, (err, val) => {
-		if (err) res.end();
+	r.db("vivi").table("connections").filter(r.row("treated").eq(false)).filter(r.row("routerId").eq(id)).changes().run(connection, (err, val) => {
+		if (err) throw err;
 		val.each((err, e) => {
-			if (err) res.end();
-			res.write("data: " + JSON.stringify(e.new_val) + "\n\n");
+			if (err) throw err;
+			const val = e.new_val !== null ? e.new_val : (e.old_val !== null ? e.old_val : null);
+			if (val !== null) res.write(`event:new\ndata: ${JSON.stringify(val)}\n\n`);
 		});
 	});
-	res.on("close", async() => {
-		res.end();
-	});
+
 });
 
 router.post("/publish/:id", checkAuthentication, async(req, res) => {
