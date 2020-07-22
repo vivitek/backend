579433146c718b068f1c0b5c753c89d12220fd87
Théo MARINHO <theo.marinho@epitech.eu>
2020-07-22 20:02:02 +0200

    [add] make admin great again

--- server/middleware/permission.js
+++ server/middleware/permission.js
@@ -4,19 +4,23 @@ const roleModel = require("../models/Role");
 
 async function checkPermission(req, res, next) {
 	let role;
-	if (process.env.DEBUG && !req.body.isTestingAuth)
+	if ((process.env.DEBUG && !req.body.isTestingAuth) || req.user.role._id === process.env.SUPER_ADMIN_ROLE_ID)
 		return next();
 	try {
 		const token = req.headers.authorization.split(" ")[1];
+		console.log("a");
 		const decodedUser = jwt.decode(token, "lifebeforedeath");
+		console.log("b");
 		const user = await userModel.findById(decodedUser._id).populate("role");
+		console.log("c");
 		role = await roleModel.findById(user.role._id).populate("permissions");
+		console.log("d");
 	} catch (err) {
-		return res.status(401).json({message: "Invalid token"});
+		return res.status(401).json({message: "Invalid a token"});
 	}
 	const banPermission = role.permissions.find(permission => permission.url === `/${req.baseUrl.split("/")[1]}`);
 	if (!banPermission || !banPermission[req.method])
-		return res.status(401).send({message: "Invalid token"});
+		return res.status(401).send({message: "Invalid z token"});
 	next();
 }
 
