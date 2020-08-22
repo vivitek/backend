4c73383e6adb43a7520698d1167b2e37837f095c
Théo MARINHO <theo.marinho@epitech.eu>
2020-08-22 15:51:14 +0200

    [improve] type request and response in token.middleware.ts

--- server2/src/middleware/token.middleware.ts
+++ server2/src/middleware/token.middleware.ts
@@ -4,6 +4,7 @@ import { User } from "../users/schemas/user.schema";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { last } from "lodash"
+import { Request, Response } from "express"
 
 
 @Injectable()
@@ -12,7 +13,7 @@ export class MiddlewareClass {
         @InjectModel('User') private readonly userModel: Model<User>,
         private jwtService: JwtService) {}
 
-    async checkAuthentication(req, res, next: Function) {
+    async checkAuthentication(req: ViviRequest, res: Response, next: Function) {
         if (process.env.DEBUG) {
             req.user = this.userModel.create({
                 email: "support@vincipit.com",
@@ -36,5 +37,15 @@ export class MiddlewareClass {
         } catch (err) {
             return res.status(401).json({message: "Invalid token"})
         }
+    }
+}
+
+type ViviRequest = Request & {
+    user?: { [x: string]: any } | string,
+    headers: {
+        authorization?: string
+    },
+    query?: {
+        token?: string
     }
 }
