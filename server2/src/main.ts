ec46c3b7126da137bfe763b8f3ffc536c9f4d523
Matteo Gassend <matteo.gassend@epitech.eu>
2020-09-28 11:26:37 +0200

    [add]: added redoc endpoints

--- server2/src/main.ts
+++ server2/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
-
+import {RedocOptions, RedocModule} from 'nestjs-redoc'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,7 +10,14 @@ async function bootstrap() {
   app.enableCors()
   const options = new DocumentBuilder().setTitle('Vivi').setDescription("Vivi's api doc").setVersion("2.0.0").addBearerAuth().build()
   const document = SwaggerModule.createDocument(app, options)
+  const redocOptions: RedocOptions = {
+    title:"Vivi",
+    sortPropsAlphabetically: true,
+    hideDownloadButton: false,
+    hideHostname: false
+  }
   SwaggerModule.setup("api", app, document)
+  await RedocModule.setup("/docs", app, document, redocOptions)
   await app.listen(3000);
 }
 bootstrap();
