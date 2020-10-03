e74496fdd5515ad70eaa2d809f2fb8ce62351ae7
Théo MARINHO <theo.marinho@epitech.eu>
2020-10-03 17:06:41 +0200

    [add] add tag tests and dto

--- server2/src/tag/tag.controller.ts
+++ server2/src/tag/tag.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Patch } from "@nestjs/common";
+import { TagService } from "./tag.service"
+import { TagCreation, TagUpdate } from "./schemas/tag.dto";
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+    constructor(private readonly tagService: TagService) {}
+
+    @Get("/")
+    async getAll() {
+        return this.tagService.findAll()
+    }
+
+    @Get("/:id")
+    async getById(@Param("id") id: string) {
+        return this.tagService.findById(id)
+    }
+
+    @Post("/")
+    async create(@Body() content: TagCreation) {
+        return this.tagService.create(content)
+    }
+
+    @Delete("/:id")
+    async deleteById(@Param("id") id: string) {
+        return this.tagService.deleteById(id)
+    }
+
+    @Patch("/:id")
+    async updateById(@Param("id") id: string, @Body() content: TagUpdate) {
+        return this.tagService.updateById(id, content)
+    }
+}
