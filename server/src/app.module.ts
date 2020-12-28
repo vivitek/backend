42c4344d547a17c3285ef698b23ebd7b3eb43882
Matteo Gassend <matteo.gassend@epitech.eu>
2020-12-28 09:41:49 +0100

    [del] removed rethinkdb and dependencies

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,28 +6,30 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BanModule } from './ban/ban.module';
 import { TagModule } from './tag/tag.module';
-import { DatabaseModule } from './database/database.module';
 import { RouterModule } from './router/router.module';
 import { IpModule } from './ip/ip.module';
 import { ServiceModule } from './service/service.module';
 import { ConfigModule } from './config/config.module';
 import { TemplateModule } from './template/tempalte.module';
-import { ConnectionsModule } from './connections/connections.module';
 import { BeamsModule } from './beams/beams.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 const MODULES = [
   MongooseModule.forRoot(`mongodb://${process.env.MONGO}`),
+  GraphQLModule.forRoot({
+    installSubscriptionHandlers:true,
+    autoSchemaFile: true,
+    playground:true
+  }),
   BanModule,
   TagModule,
   AuthModule,
   UsersModule,
-  DatabaseModule,
   RouterModule,
   IpModule,
   ServiceModule,
   ConfigModule,
   TemplateModule,
-  ConnectionsModule,
   BeamsModule
 ]
 
--- server/src/ip/ip.module.ts
+++ server/src/ip/ip.module.ts
@@ -3,11 +3,10 @@ import { Module } from '@nestjs/common';
 import { Ip, IpSchema } from './schemas/ip.schema';
 import { IpController } from './ip.controller';
 import { IpService } from './ip.service';
-import { DatabaseModule } from '../database/database.module';
 
 @Module({
 	imports:[MongooseModule.forFeature([{ name: Ip.name, schema: IpSchema}])],
 	controllers:[IpController],
-	providers:[IpService, DatabaseModule]
+	providers:[IpService]
 })
 export class IpModule {}
