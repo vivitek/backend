8e28451b0d31455aad7bee295d3f7337fce092f1
Matteo Gassend <matteo.gassend@epitech.eu>
2021-01-24 16:07:01 +0100

    [add] added config resolver

--- server/src/config/config.controller.ts
+++ server/src/config/config.controller.ts
@@ -1,33 +1,46 @@
-import { Controller, Get, Param, Post, Body, Delete, Patch } from "@nestjs/common";
-import { ConfigService } from "./config.service"
-import { ConfigCreation, ConfigDTO, ConfigUpdate } from "./schemas/config.dto";
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { ConfigService } from './config.service';
+import { ConfigCreation, ConfigUpdate } from './schemas/config.dto';
+import {
+  ConfigCreationInput,
+  ConfigUpdateInput,
+} from './schemas/config.inputs';
+import { Config } from './schemas/config.schema';
 
 @Controller('config')
 export class ConfigController {
-    constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
 
-    @Get("/")
-    async getAll()/*: Promise<Array<ConfigDTO>>*/ {
-        return this.configService.findAll()
-    }
+  @Get('/')
+  async getAll(): Promise<Array<Config>> {
+    return this.configService.findAll();
+  }
 
-    @Get("/:id")
-    async getById(@Param("id") id: string)/*: Promise<ConfigDTO> */ {
-        return this.configService.findById(id)
-    }
+  @Get('/:id')
+  async getById(@Param('id') id: string): Promise<Config> {
+    return this.configService.findById(id);
+  }
 
-    @Post("/")
-    async create(@Body() content: ConfigCreation)/*: Promise<ConfigDTO> */ {
-        return this.configService.create(content)
-    }
+  @Post('/')
+  async create(@Body() content: ConfigCreationInput): Promise<Config> {
+    return this.configService.create(content);
+  }
 
-    @Delete("/:id")
-    async deleteById(@Param("id") id: string)/*: Promise<ConfigDTO> */ {
-        return this.configService.deleteById(id)
-    }
+  @Delete('/:id')
+  async deleteById(@Param('id') id: string): Promise<Config> {
+    return this.configService.deleteById(id);
+  }
 
-    @Patch("/:id")
-    async updateById(@Param("id") id: string ,@Body() content: ConfigUpdate)/*: Promise<ConfigDTO> */ {
-        return this.configService.updateById(id, content)
-    }
+  @Patch('/:id')
+  async updateById(@Body() content: ConfigUpdateInput): Promise<Config> {
+    return this.configService.updateById(content);
+  }
 }
