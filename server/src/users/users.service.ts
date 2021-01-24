b02036adf68d15c9baaeea958d358a847babcf12
Matteo Gassend <matteo.gassend@epitech.eu>
2021-01-24 22:20:34 +0100

    [fix] user models and service are up to new standards

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -3,40 +3,41 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/users.schema';
 import { Model } from 'mongoose';
-import * as bcrypt from "bcrypt"
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-	@InjectModel(User.name) private userModel: Model<User>
+  @InjectModel(User.name) private userModel: Model<User>;
 
-	async findById(id:string): Promise<User> {
-		return this.userModel.findById(id)
-	}
-	async findOne(email: string): Promise<User | undefined> {
-		return this.userModel.findOne({email});
-	}
-	async getAll() : Promise<User[]> {
-		return this.userModel.find()
-	}
-	async createUser(email:string, password:string, username:string): Promise<User | undefined> {
-		return this.userModel.create({email:email, password:bcrypt.hashSync(password, 12), username:username})
-	}
-	async deleteUser(id: string): Promise<User> {
-		return this.userModel.findByIdAndDelete(id)
-	}
-	async updateUser(id:string, data:UserDto): Promise<User> {
-		const user = await this.userModel.findById(id)
-		if (data.email)
-			user.email = data.email
-		if (data.username)
-			user.username = data.username
-		const d = await user.save()
-		return d
-	}
-
-	async deleteAll()/*: Promise<Array<User>> */{
-		if (!process.env.DEBUG) return
-		return this.userModel.db.dropDatabase()
-	}
-
+  async findById(id: string): Promise<User> {
+    return this.userModel.findById(id);
+  }
+  async findOne(email: string): Promise<User | undefined> {
+    return this.userModel.findOne({ email });
+  }
+  async getAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+  async createUser(
+    email: string,
+    password: string,
+    username: string,
+  ): Promise<User | undefined> {
+    const user = new this.userModel({
+      email: email,
+      password: bcrypt.hashSync(password, 12),
+      username: username,
+    });
+    return user.save();
+  }
+  async deleteUser(id: string): Promise<User> {
+    return this.userModel.findByIdAndDelete(id);
+  }
+  async updateUser(id: string, data: User): Promise<User> {
+    const user = await this.userModel.findById(id);
+    if (data.email) user.email = data.email;
+    if (data.username) user.username = data.username;
+    const d = await user.save();
+    return d;
+  }
 }
