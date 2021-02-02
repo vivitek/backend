ff8a13d9163e28c4fb73111d53ef88152b626b2e
Matteo Gassend <matteo.gassend@epitech.eu>
2021-02-02 10:58:20 +0100

    [imp] refactored service to use inputs as graphql

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/users/schemas/users.schema';
+import { AuthDetails } from './schema/auth.schema';
+import { LoginInput, RegisterInput } from './schema/auth.inputs';
 
 @Injectable()
 export class AuthService {
@@ -11,18 +13,15 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, password: string): Promise<User | null> {
+  async validateUser({ email, password }: LoginInput): Promise<User | null> {
     const user = await this.usersService.findByEmail(email);
     if (user && bcrypt.compareSync(password, user.password)) {
       return user;
     }
     return null;
   }
-  async login(
-    email: string,
-    password: string,
-  ): Promise<{ access_token: string; user: User }> {
-    const payload = await this.validateUser(email, password);
+  async login(data: LoginInput): Promise<AuthDetails> {
+    const payload = await this.validateUser(data);
     if (payload) {
       return {
         access_token: this.jwtService.sign(payload.toJSON(), {
@@ -31,13 +30,13 @@ export class AuthService {
         user: payload,
       };
     }
-    return null;
+    throw new BadRequestException('Something went wrong');
   }
-  async register(
-    email: string,
-    password: string,
-    username: string,
-  ): Promise<{ access_token: string; user: User }> {
+  async register({
+    email,
+    username,
+    password,
+  }: RegisterInput): Promise<AuthDetails> {
     try {
       const user = await this.usersService.createUser({
         email,
