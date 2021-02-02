221de0c0a9f8966fc56526a1c93062edbc99972e
Matteo Gassend <matteo.gassend@epitech.eu>
2021-02-02 10:46:05 +0100

    [add] added schemas for input/output auth service

---
+++ server/src/auth/schema/auth.inputs.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  username: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+@InputType()
+export class LoginInput {
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
