0dcb4b95bece47e0fbc53ea77dde56a9ca931112
Théo MARINHO <theo.marinho@epitech.eu>
2021-04-18 14:56:10 +0200

    [add] add balena service resolver and utils

--- server/src/balena/balena.module.ts
+++ server/src/balena/balena.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BalenaDeviceResolver } from './balena.resolver';
 import { BalenaService } from './balena.service';
+import { BalenaUtils } from './balena.utils';
 
 @Module({
-  providers: [BalenaService],
+  providers: [BalenaService, BalenaUtils, BalenaDeviceResolver],
   exports: [BalenaService],
 })
 export class BalenaModule {}
--- server/src/balena/balena.service.ts
+++ server/src/balena/balena.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { BalenaUtils } from './balena.utils';
+import { BalenaDevice, BalenaDeviceEnvVar } from './schema/balena.schema';
 
 @Injectable()
 export class BalenaService {
-  async getDevices() {
-    return;
+  public constructor(private balenaUtils: BalenaUtils) {}
+
+  public async getRouters(): Promise<BalenaDevice[]> {
+    return await this.balenaUtils.getRouters()
   }
 
-  async getApplicationInfo() {
-    return;
+  public async getRouterByUuid(uuid: string): Promise<BalenaDevice> {
+    const device = await this.balenaUtils.getByUuid(uuid)
+    const env = await this.balenaUtils.getEnvVar(device.id)
+    return {...device, env}
   }
 
-  async setEnvOnDevice() {
-    return;
+  public async getEnvVarByDeviceId(id: string): Promise<BalenaDeviceEnvVar[]> {
+    return await this.balenaUtils.getEnvVar(id)
   }
 
-  async getDeviceInfo() {
-    return;
+  public async setEnvVarByUuid(uuid: string, key: string, value: string) {
+    const device = await this.getRouterByUuid(uuid)
+    return await this.balenaUtils.setEnvVar(device.id, key, value)
+  }
+
+  public async delEnvVarByUuid(variableId: string) {
+    return await this.balenaUtils.delEnvVar(variableId)
   }
 }
