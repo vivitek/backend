f4ac4e2bd72f798def28d8ef52958790d0889626
Théo MARINHO <theo.marinho@epitech.eu>
2021-05-03 19:21:14 +0200

    [add] add box auth handling

--- server/src/router/router.module.ts
+++ server/src/router/router.module.ts
@@ -3,10 +3,17 @@ import { Module } from '@nestjs/common';
 import { Router, RouterSchema } from './schemas/router.schema';
 import { RouterService } from './router.service';
 import { RouterResolver } from './router.resolver';
+import { BalenaModule } from '../balena/balena.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Router.name, schema: RouterSchema }]),
+    BalenaModule,
+    JwtModule.register({
+      secret: jwtConstants.secret
+    })
   ],
   providers: [RouterService, RouterResolver],
 })
