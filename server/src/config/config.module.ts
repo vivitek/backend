8e1c6cdfb5c092914414b37e3453e0acbe23d871
Matteo Gassend <matteo.gassend@epitech.eu>
2021-11-30 14:06:35 +0100

    [fix] user reslver for config

--- server/src/config/config.module.ts
+++ server/src/config/config.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { Config, ConfigSchema } from './schemas/config.schema';
 import { ConfigService } from './config.service';
 import { ConfigResolver } from './config.resolver';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Config.name, schema: ConfigSchema }]),
+    UsersModule,
   ],
   providers: [ConfigService, ConfigResolver],
 })
